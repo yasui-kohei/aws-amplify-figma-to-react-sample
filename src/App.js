979bec034a5a59b59b533e2fa1b15ddba9347326
yasui-kohei <yasui.kohei.xj@alumni.tsukuba.ac.jp>
2023-07-15 18:52:01 +0900

    [実装] aws amplifyのコンポーネント取り込み 安井康平

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Spacer } from "./manual-components/spacer";
+import { ActionCard, ItemCard } from "./ui-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ justifyContent: "center", alignItems: "center" }}
+    >
+      <ItemCard
+        backgroundColor={"yellow.40"}
+        borderRadius={10}
+        title={"Hello"}
+        subTitle={"develop Hello Company"}
+        price={300}
+        isNew={true}
+      />
+
+      <Spacer size={20} />
+      
+      <ActionCard
+        backgroundColor={"brand.primary.10"}
+        title={"OfficialYKS"}
+        description={
+          "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J"
+        }
+        rating={4}
+      />
     </div>
   );
 }
